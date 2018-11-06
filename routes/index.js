c2405e5a5894a353e27d17a45ab868a8ccf599ce
santanu <edu.santanu12@gmail.com>
2018-11-07 00:46:45 +0530

    admin post page user side templates aded

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET login page. */
-router.get('/adminlogin', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET front page. */
+router.get('/', function(req, res, next) {
+  res.render('./user/index');
+});
+
+// about us
+router.get('/contact_us', function(req, res, next) {
+  res.render('./user/Contact_us');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET users login */
+router.get('/login', function(req, res, next) {
+  res.render('./admin/login');
 });
-
+// post blog
+router.get('/post', function(req, res, next) {
+  res.render('./admin/post');
+});
 module.exports = router;
